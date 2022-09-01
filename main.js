d69636cc41836e83b4b6a42abeae441c72f920d3
Pythonikk <nikkibrusoe@gmail.com>
2022-09-01 15:17:35 -0700

    Finish implementing adding a new book to library.

--- main.js
+++ main.js
@@ -30,40 +30,48 @@ exitFormBtn.addEventListener('click', event => {
   toggleNewBookForm();
 })
 
+const addBookBtn = document.querySelector('.submitBook');
+addBookBtn.addEventListener('click', event => {
+  // preventDefault to keep the from from submitting thus refreshing page.
+  event.preventDefault();
+  toggleNewBookForm();
+  addBookToLibrary(document.getElementsByName('title')[0].value, document.getElementsByName('author')[0].value, document.getElementsByName('pages')[0].value, document.getElementsByName('read')[0].value);
+  createBookCard(library.at(-1));
+})
+
+function createBookCard(book) {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'book');
+  div.style.boxShadow = "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px";
+  div.style.backgroundColor = "#ffe6e6";
+  div.style.padding = "10px";
+  container.appendChild(div);
+
+  const para = document.createElement('p');
+  para.textContent = `Title: ${book.title}`;
+  div.appendChild(para);
+  const para2 = document.createElement('p');
+  para2.textContent = `By: ${book.author}`;
+  div.appendChild(para2);
+  const para3 = document.createElement('p');
+  para3.textContent = `Pages: ${book.pages}`;
+  div.appendChild(para3);
+  const para4 = document.createElement('p');
+  if (book.read === 'true') {
+    para4.textContent = 'Have Read'
+  } else {
+    para4.textContent = 'Have Not Read'
+  };
+  div.appendChild(para4);
+}
+
 function displayBooks() {
   library.forEach(book => {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'book');
-    div.style.boxShadow = "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px";
-    div.style.backgroundColor = "#ffe6e6";
-    div.style.padding = "10px";
-    container.appendChild(div);
-
-    const para = document.createElement('p');
-    para.textContent = `Title: ${book.title}`;
-    div.appendChild(para);
-
-    const para2 = document.createElement('p');
-    para2.textContent = `By: ${book.author}`;
-    div.appendChild(para2);
-
-    const para3 = document.createElement('p');
-    para3.textContent = `Pages: ${book.pages}`;
-    div.appendChild(para3);
-
-    const para4 = document.createElement('p');
-    if (book.read === true) {
-      para4.textContent = 'Have Read'
-    } else {
-      para4.textContent = 'Have Not Read'
-    };
-    div.appendChild(para4);
+    createBookCard(book);
   });
 }
 
-addBookToLibrary('The Ramayana', 'Valmiki', '360', true);
-addBookToLibrary('Parzival', 'Wolfram Von Eschenback', '436', true);
-addBookToLibrary('Eloquent Ruby', 'Russ Olsen', '400', true);
+addBookToLibrary('The Ramayana', 'Valmiki', '360', 'true');
+addBookToLibrary('Parzival', 'Wolfram Von Eschenback', '436', 'true');
+addBookToLibrary('Eloquent Ruby', 'Russ Olsen', '400', 'true');
 displayBooks();
-
-
