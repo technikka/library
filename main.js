7f4ea8ba3f21c74563f77f36b8e18151622b2462
Pythonikk <nikkibrusoe@gmail.com>
2022-09-02 13:07:20 -0700

    Add functionality for removing a book from library, and add some styling.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let library = [];
 const container = document.querySelector('.books-container');
 
 function Book(title, author, pages, read) {
+  this.id = library.length;
   this.title = title
   this.author = author
   this.pages = pages
@@ -41,21 +42,21 @@ addBookBtn.addEventListener('click', event => {
 
 function createBookCard(book) {
   const div = document.createElement('div');
-  div.setAttribute('class', 'book');
-  div.style.boxShadow = "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px";
-  div.style.backgroundColor = "#ffe6e6";
-  div.style.padding = "10px";
+  div.setAttribute('class', 'book-card');
   container.appendChild(div);
 
   const para = document.createElement('p');
-  para.textContent = `Title: ${book.title}`;
+  para.textContent = `${book.title}`;
+  para.style.fontWeight = "bolder";
   div.appendChild(para);
+
   const para2 = document.createElement('p');
-  para2.textContent = `By: ${book.author}`;
+  para2.textContent = `${book.author}`;
   div.appendChild(para2);
   const para3 = document.createElement('p');
-  para3.textContent = `Pages: ${book.pages}`;
+  para3.textContent = `${book.pages} pages`;
   div.appendChild(para3);
+
   const para4 = document.createElement('p');
   if (book.read === 'true') {
     para4.textContent = 'Have Read'
@@ -63,6 +64,24 @@ function createBookCard(book) {
     para4.textContent = 'Have Not Read'
   };
   div.appendChild(para4);
+
+  const deleteBookBtn = document.createElement('button');
+  deleteBookBtn.dataset.bookIndex = book.id;
+  deleteBookBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m18.15 32.9 5.9-6 5.95 6 2.25-2.3-5.9-6 5.9-6L30 16.25l-5.95 6-5.9-6-2.35 2.35 5.95 6-5.95 6Zm-5 8.85q-1.15 0-2.025-.875t-.875-2.025v-28.3h-2V7.7h9.2V6.25H30.6V7.7h9.2v2.85h-2v28.3q0 1.15-.875 2.025t-2.025.875Zm21.75-31.2H13.15v28.3H34.9Zm-21.75 0v28.3Z"/></svg>';
+  deleteBookBtn.setAttribute('title', 'Delete From Library');
+  deleteBookBtn.setAttribute('class', 'deleteBookBtn');
+  deleteBookBtn.addEventListener('click', event => {
+    deleteBookFromLibrary(book, deleteBookBtn);
+  });
+  div.appendChild(deleteBookBtn);
+}
+
+function deleteBookFromLibrary(book, div) {
+  const findBookID = (element) => element.id === book.id;
+  indexOfBook = library.findIndex(findBookID);
+  library.splice(indexOfBook, 1);
+  cardToRemove = div.parentElement;
+  cardToRemove.remove();
 }
 
 function displayBooks() {
