8e793612b96f1be274ae6e4a4d5a9cb5237470fe
Pythonikk <nikkibrusoe@gmail.com>
2022-09-22 14:41:13 -0700

    Add BookCard class and all methods to create its elements.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class Library {
 
   displayBooks() {
     this.books.forEach(book => {
-      createBookCard(book);
+      new BookCard(book);
     });
   }
 
@@ -44,7 +44,85 @@ class Book {
   }
 }
 
-const container = document.querySelector('.books-container');
+class BookCard {
+
+  constructor(book) {
+    this.book = book;
+    this.div = this.createDiv();
+    this.addTitle();
+    this.addAuthor();
+    this.addPages();
+    const pReadStatus = this.addReadStatus();
+    this.addChangeStatusBtn(pReadStatus);
+    this.addDeleteBtn()
+  }
+
+  createDiv() {
+    const container = document.querySelector('.books-container');
+    const div = document.createElement('div');
+    div.setAttribute('class', 'book-card');
+    container.appendChild(div);
+    return div;
+  }
+
+  addTitle() {
+    let para = document.createElement('p');
+    para.textContent = `${this.book.title}`;
+    para.style.fontWeight = "bolder";
+    this.div.appendChild(para);
+  }
+
+  addAuthor() {
+    let para = document.createElement('p');
+    para.textContent = `${this.book.author}`;
+    this.div.appendChild(para);
+  }
+
+  addPages() {
+    let para = document.createElement('p');
+    para.textContent = `${this.book.pages} pages`;
+    this.div.appendChild(para);
+  }
+
+  _toggleReadText(book, text) {
+    if (book.read === 'true') {
+      text.textContent = 'Read';
+    } else {
+      text.textContent = 'Not Read yet';
+    };
+  }
+
+  addReadStatus() {
+    let para = document.createElement('p');
+    this._toggleReadText(this.book, para);
+    this.div.appendChild(para);
+    return para;
+  }
+
+  addChangeStatusBtn(readStatus) {
+    const readStatusBtn = document.createElement('button');
+    readStatusBtn.setAttribute('class', 'readStatusBtn');
+    readStatusBtn.textContent = 'Change Status';
+    this.div.appendChild(readStatusBtn);
+    readStatusBtn.addEventListener('click', event => {
+      this.book.toggleReadStatus();
+      this._toggleReadText(this.book, readStatus);
+    })
+  }
+
+  addDeleteBtn() {
+    const deleteBookBtn = document.createElement('button');
+    deleteBookBtn.dataset.bookIndex = this.book.id;
+    deleteBookBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path   d="m18.15 32.9 5.9-6 5.95 6 2.25-2.3-5.9-6 5.9-6L30 16.25l-5.95 6-5.9-6-2.35 2.35 5.95 6-5.95   6Zm-5 8.85q-1.15 0-2.025-.875t-.875-2.025v-28.3h-2V7.7h9.2V6.25H30.6V7.7h9.2v2.85h-2v28.3q0 1.15-.  875 2.025t-2.025.875Zm21.75-31.2H13.15v28.3H34.9Zm-21.75 0v28.3Z"/></svg>';
+    deleteBookBtn.setAttribute('title', 'Delete From Library');
+    deleteBookBtn.setAttribute('class', 'deleteBookBtn');
+    deleteBookBtn.addEventListener('click', event => {
+      library.deleteBook(this.book, deleteBookBtn);
+    });
+    this.div.appendChild(deleteBookBtn);
+  }
+
+}
 
 const modalFocusableElments = [document.querySelector('.exit-btn'),
                                document.getElementsByName('title')[0],
@@ -100,57 +178,8 @@ addBookBtn.addEventListener('click', event => {
   event.preventDefault();
   toggleNewBookForm();
   library.addBook(document.getElementsByName('title')[0].value, document.getElementsByName('author')[0].value, document.getElementsByName('pages')[0].value, document.getElementsByName('read')[0].value);
-  createBookCard(library.books.at(-1));
+  new BookCard(library.books.at(-1));
 })
-
-function toggleReadText(book, text) {
-  if (book.read === 'true') {
-    text.textContent = 'Read';
-  } else {
-    text.textContent = 'Not Read yet';
-  };
-}
-
-function createBookCard(book) {
-  const div = document.createElement('div');
-  div.setAttribute('class', 'book-card');
-  container.appendChild(div);
-
-  const para = document.createElement('p');
-  para.textContent = `${book.title}`;
-  para.style.fontWeight = "bolder";
-  div.appendChild(para);
-
-  const para2 = document.createElement('p');
-  para2.textContent = `${book.author}`;
-  div.appendChild(para2);
-  const para3 = document.createElement('p');
-  para3.textContent = `${book.pages} pages`;
-  div.appendChild(para3);
-
-  const para4 = document.createElement('p');
-  toggleReadText(book, para4);
-  div.appendChild(para4);
-
-  const readStatusBtn = document.createElement('button');
-  readStatusBtn.setAttribute('class', 'readStatusBtn');
-  readStatusBtn.textContent = 'Change Status';
-  div.appendChild(readStatusBtn);
-  readStatusBtn.addEventListener('click', event => {
-    book.toggleReadStatus();
-    toggleReadText(book, para4);
-  })
-
-  const deleteBookBtn = document.createElement('button');
-  deleteBookBtn.dataset.bookIndex = book.id;
-  deleteBookBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="m18.15 32.9 5.9-6 5.95 6 2.25-2.3-5.9-6 5.9-6L30 16.25l-5.95 6-5.9-6-2.35 2.35 5.95 6-5.95 6Zm-5 8.85q-1.15 0-2.025-.875t-.875-2.025v-28.3h-2V7.7h9.2V6.25H30.6V7.7h9.2v2.85h-2v28.3q0 1.15-.875 2.025t-2.025.875Zm21.75-31.2H13.15v28.3H34.9Zm-21.75 0v28.3Z"/></svg>';
-  deleteBookBtn.setAttribute('title', 'Delete From Library');
-  deleteBookBtn.setAttribute('class', 'deleteBookBtn');
-  deleteBookBtn.addEventListener('click', event => {
-    library.deleteBook(book, deleteBookBtn);
-  });
-  div.appendChild(deleteBookBtn);
-}
 
 let library = new Library();
 library.addBook('The Ramayana', 'Valmiki', '360', 'true');
